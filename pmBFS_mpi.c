2a8bff4808227ddec9524c7a02f5beaa5c02f150
Rodrick <rodkd@live.com>
2017-04-28 15:48:05 -0400

    completely fixed code, runs slowly though

--- pmBFS_mpi.c
+++ pmBFS_mpi.c
@@ -146,12 +146,12 @@ void mat_vec_mul(long double prod[3], long double vec[3], long double mat[3][3])
 
 /* Run simulation */
 // A[i][j] keeps track of allowable transitions between i and j
-void BFS(long double A[P_SIZE][5], long double P[P_SIZE][6], int world_rank, int world_size,int m) {
+void BFS(long double A[P_SIZE][5], long double P[P_SIZE][5], int world_rank, int world_size,int m) {
   // omp_set_num_threads(91);
   int rank;
   //omp_set_num_threads(91);
   long int lower, upper, i, j,k,size,width,start,end;
-  long double holder[6];
+  long double holder[5];
   MPI_Request request;
   start = 0;
   end = 1;
@@ -171,16 +171,16 @@ void BFS(long double A[P_SIZE][5], long double P[P_SIZE][6], int world_rank, int
     //for (i = start; i < end; i++){
   for (i = start+(long int) round((k)*(width)/(1.0*size)); i < start+ (long int) round((world_rank+1.0)*width/(1.0*size)); i++) {
     // lower bound of particles we can move to
-    if (m > 0){
-      rank = (int) floor((floor((i-1.0)/12.0)-(start-pow(12,m-1)))*1.0*size/(1.0*(width-pow(12,m)+pow(12,m-1))));
-      MPI_Irecv(holder,6,MPI_LONG_DOUBLE,rank,world_rank,MPI_COMM_WORLD,&request);
-      P[i][0] = holder[0];
-      P[i][1] = holder[1];
-      P[i][2] = holder[2];
-      P[i][3] = holder[3];
-      P[i][4] = holder[4];
-      P[i][5] = holder[5];
-    }
+    //    if (m > 0){
+    //rank = (int) floor((floor((i-1.0)/12.0)-(start-pow(12,m-1)))*1.0*size/(1.0*(width-pow(12,m)+pow(12,m-1))));
+    //MPI_Irecv(holder,5,MPI_LONG_DOUBLE,rank,world_rank,MPI_COMM_WORLD,&request);
+    //P[i][0] = holder[0];
+    //P[i][1] = holder[1];
+    //P[i][2] = holder[2];
+    //P[i][3] = holder[3];
+    //P[i][4] = holder[4];
+      //     P[i][5] = holder[5];
+    
     lower = ((12*i+1) < P_SIZE) ? (12*i+1) : P_SIZE;
     //  if (lower == P_SIZE) {
     //   break;
@@ -189,14 +189,14 @@ void BFS(long double A[P_SIZE][5], long double P[P_SIZE][6], int world_rank, int
     upper = ((12*i+13) <= P_SIZE) ? (12*i+13) : P_SIZE;
     // iterate through the 12 possible lattice movements
     for (j = lower; j < upper; j++) {
-      if (P[i][5] == 1.0) {
+      //      if (P[i][5] == 1.0) {
 	// [0] is x position
 	P[j][0] = A[j][0]+P[i][0];
 	// [5] keeps track of where we visited
 	//   *ii = *ii+1;
-	if ((j % 12) == 0) {
-	  P[i][5] += 1.0;
-	}
+	//if ((j % 12) == 0) {
+	//  P[i][5] += 1.0;
+	//}
 	// [1] is y position
 	P[j][1] = A[j][1]+P[i][1];
 	// [2] is z position
@@ -205,20 +205,36 @@ void BFS(long double A[P_SIZE][5], long double P[P_SIZE][6], int world_rank, int
 	P[j][3] = A[j][3]+P[i][3];
 	// [4] is probability
 	P[j][4] = A[j][4]*P[i][4];
-	P[j][5] = 1.0;
-	holder[0] = P[j][0];
-	holder[1] = P[j][1];
-	holder[2] = P[j][2];
-	holder[3] = P[j][3];
-	holder[4] = P[j][4];
-	holder[5] = P[j][5];
-	if (m < N-1){
-	rank = (int) floor((j-(start+pow(12,m)))*1.0*size/(1.0*(width+pow(12,m+1)-pow(12,m))));
-	MPI_Isend(holder,6,MPI_LONG_DOUBLE,rank,rank,MPI_COMM_WORLD,&request);
-	}
-      }
+	//	P[j][5] = 1.0;
+	//	holder[5] = P[j][5];
+	//if (m < N-1){
+	  //rank = (int) floor((j-(start+pow(12,m)))*1.0*size/(1.0*(width+pow(12,m+1)-pow(12,m))));
+	//MPI_Isend(holder,5,MPI_LONG_DOUBLE,rank,rank,MPI_COMM_WORLD,&request);
+	//}
+	//}
     }
   }
+    if (m < N-1){
+    for (i = start; i < end; i++) {
+    lower = 12*i+1;
+    upper = 12*i+13;
+    for (j = lower; j < upper; j++) {
+    
+      holder[0] = P[j][0];
+      holder[1] = P[j][1];
+      holder[2] = P[j][2];
+      holder[3] = P[j][3];
+      holder[4] = P[j][4];
+      rank = (int) floor((i-(start))*1.0*size/(1.0*(width)));
+    MPI_Bcast(holder,5,MPI_LONG_DOUBLE,world_rank,MPI_COMM_WORLD);
+      P[j][0] = holder[0];
+      P[j][1] = holder[1];
+      P[j][2] = holder[2];
+      P[j][3] = holder[3];
+      P[j][4] = holder[4];
+    }	
+    }
+    }
   /*for (i = start; i < start+width; i++) {
     // lower bound of particles we can move to
     lower = ((12*i+1) < P_SIZE) ? (12*i+1) : P_SIZE;
@@ -263,7 +279,7 @@ int main(int argc, char** argv) {
   long double Q1, Q2;
   long int i, j, k,kk,l,m, width,size,endd,startt;
 
-  long double (*P)[6];
+  long double (*P)[5];
   long double (*P2)[5];
   long double (*A)[5];
   long double rotmf[3][3];
@@ -279,7 +295,7 @@ int main(int argc, char** argv) {
   Q1 = 6.*r1+3.*r3+3.*r4+3.*r5;
   // starting from hexagonal site
   Q2 = 6.*r2+3.*r3+3.*r4+3.*r5;
-  P = malloc(P_SIZE * sizeof(long double[6]));
+  P = malloc(P_SIZE * sizeof(long double[5]));
   P2 = malloc(P_SIZE * sizeof(long double[5]));
   A = malloc(P_SIZE * sizeof(long double[5]));
   int *checker;
@@ -361,22 +377,22 @@ int main(int argc, char** argv) {
   for (i = 0; i < 3; i++) {
     memcpy(&cell2[i], &cell2_duplicate[i], sizeof(cell2_duplicate[0]));
   }
-  l = 0;
+  //l = 0;
   // initializes all elements to 0
-  while(end-start<10)
-      {
+  //while(end-start<10)
+  //    {
   for (i = 0; i < P_SIZE; i++) {
     // prevents duplicating probabilities
     checker[i] = 0;
     }
   swtcher[0] = 0;
-   end = time(NULL);
-  l++;
-  }
-  if(world_rank==0)
-   {
-   printf("time 1: %.11Lf, %ld\n",(long double)(end-start)/(1.0*l),l);
-   }
+  // end = time(NULL);
+  //l++;
+  //}
+  //if(world_rank==0)
+  // {
+  // printf("time 1: %.11Lf, %ld\n",(long double)(end-start)/(1.0*l),l);
+  // }
   //omp_set_num_threads(91);
   for (k = 0; k < 12; k++) {
     i = 0;
@@ -393,6 +409,7 @@ int main(int argc, char** argv) {
       A[n_index][3] = 1./Q1;
       A[n_index][4] = r1/Q1;
       swtcher[n_index] = swtcher[i];
+      	
     }
     // below plane
     else if (k < 9) {
@@ -413,11 +430,11 @@ int main(int argc, char** argv) {
       swtcher[n_index] = (swtcher[i]+1)%4;
     }
   }
-  start = time(NULL);
-  end = time(NULL);
-  l = 0;
-  while (end-start <10)
-  {
+  //start = time(NULL);
+  //end = time(NULL);
+  //l = 0;
+  //while (end-start <10)
+  //{
   for (m = 1; m < N; m++) {
     startt = 0;
     endd = 1;
@@ -530,49 +547,50 @@ int main(int argc, char** argv) {
       }
     }
   }
-  l++;
-  end = time(NULL);
-  }
-  if (world_rank == 0)
-    {
-     printf("time 2: %.11Lf\n",(long double)(end-start)/(1.0*l)); 
-    }
+  //l++;
+  //end = time(NULL);
+  //}
+  //if (world_rank == 0)
+  // {
+  // printf("time 2: %.11Lf\n",(long double)(end-start)/(1.0*l)); 
+  // }
+  P[0][0] = 0.0;
   P[0][1] = 0.0;
   P[0][2] = 0.0;
   P[0][3] = 0.0;
   P[0][4] = 1.0;
-  P[0][5] = 1.0;
+  //  P[0][5] = 1.0;
   //ii = 0;
   
   //  if (world_rank == 0){
   //printf("%Lf\n", P[0][4]);
   //}
-  start = time(NULL);
-  end = time(NULL);
-  l = 0;
-  while(end-start <10)
-  {
+  //start = time(NULL);
+  //end = time(NULL);
+  //l = 0;
+  //while(end-start <10)
+  //{
   for (i = 0; i < N; i++) {
   BFS(A, P,world_rank,world_size,i);
   }
-  end = time(NULL);
-  l++;
-   }
-  if (world_rank == 0)
-   {
-   printf("time 3: %.11Lf\n",(long double)(end-start)/(1.0*l)); 
-  }
+  //end = time(NULL);
+  //l++;
+  //}
+  //if (world_rank == 0)
+  // {
+  // printf("time 3: %.11Lf\n",(long double)(end-start)/(1.0*l)); 
+  //}
   
   //printf("%ld\n",ii);
-  n_index = N % 2;
+  //  n_index = N % 2;
   
-  start = time(NULL);
-  end = time(NULL);
-  l = 0;
-  while (end-start < 10)
-    {
+  //start = time(NULL);
+  //end = time(NULL);
+  //l = 0;
+  //while (end-start < 10)
+    // {
   for (i = 0; i < P_SIZE; i++) {
-    P2[i][4] = 0.0;
+    P2[i][4] = -1.0;
     if (checker[i] == 1){
       continue;
     }
@@ -583,17 +601,17 @@ int main(int argc, char** argv) {
     //width = P_SIZE - i;
     //size = world_size < width ? world_size : width;
     //kk = world_rank > (width-1) ? (long int) world_size: (long int) world_rank;
-#pragma omp parallel for default(shared) private(j) schedule(static) reduction(+:sum)
+#pragma omp parallel for schedule(static) reduction(+:sum)
     for (j = i; j < P_SIZE; j++) {
     //for (j = i+(long int) round((kk)*(width)/(1.0*size)); j < i+ (long int) round((world_rank+1.0)*width/(1.0*size)); j++) {
       if (checker[j] == 0){
 	if ((fabsl(P[j][3] - P[i][3])<0.0000001 && fabsl(P[j][0] - P[i][0])<0.0000001) &&
 	  (fabsl(P[j][1] - P[i][1])<0.0000001 && fabsl(P[j][2] - P[i][2])<0.0000001)) {
 	if (m == 0){
-	  P2[i][0] = P[i][0];
-	  P2[i][1] = P[i][1];
-	  P2[i][2] = P[i][2];
-	  P2[i][3] = P[i][3];
+	  P2[i][0] = P[j][0];
+	  P2[i][1] = P[j][1];
+	  P2[i][2] = P[j][2];
+	  P2[i][3] = P[j][3];
 	  m++;
 	}
 	sum += P[j][4];
@@ -608,22 +626,24 @@ int main(int argc, char** argv) {
 //    }
 //   MPI_Reduce(&sum,&gsum,1,MPI_LONG_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);	
     P2[i][4] = sum;
+    if (world_rank == 0)
+      {
+	if (P2[i][4] > 0.0) {
+  	  printf("%Lf %Lf %Lf %Lf %.11Lf\n", P2[i][0], P2[i][1], P2[i][2], P2[i][3], P2[i][4]);
+  	}
+      }
+   
   }
-  l++;
-  end = time(NULL);
+  //l++;
+  //end = time(NULL);
   //if (world_rank == 0)
   // {
   //  clock_gettime(CLOCK_MONOTONIC,&end);
   //}
-    }
-   if (world_rank ==0)
-   {
-     printf("time: %.11Lf\n",(long double) (end-start)/(1.0*l));
-   }
-  //if (P2[i][4] > 0.0) {
-  //	  printf("%Lf %Lf %Lf %Lf %.11Lf\n", P2[i][0], P2[i][1], P2[i][2], P2[i][3], P2[i][4]);
-  //	}
-
+//if (world_rank ==0)
+// {
+//   printf("time: %.11Lf\n",(long double) (end-start)/(1.0*l));
+// }
    //  if (world_rank == 0) {
    //printf("\n%.11Lf %ld\n", (long double)((end.tv_sec+end.tv_nsec*1.0e-9-start.tv_sec-start.tv_nsec*1.0e-9)),l);
    //}
